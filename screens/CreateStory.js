936acadbc66a275df48c26e23d1ba5ee0fc462bc
VADLURIVIDHATH <111587094+VADLURIVIDHATH@users.noreply.github.com>
2023-07-14 21:00:11 +0530

    Add files via upload

---
+++ screens/CreateStory.js
@@ -0,0 +1,43 @@
+import React, {Component}from 'react';
+import {Text,View}from 'react-native';
+
+export default class CreateStory extends component{
+render(){
+
+    return(
+
+<View
+style={{
+ flex:1,
+ justifyContent:"center",
+ alignItems:"center"   
+}} >
+
+<Text> CreateStory   </Text> 
+     
+</View>   
+
+    )
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ screens/Profile.js
@@ -0,0 +1,20 @@
+import React,{Component}from 'react';
+import{View,Text}from 'react-native';
+
+export default class Profile extends component{
+return(){
+
+    return(
+ <view
+ style={{
+flex:"1",
+justifyContent:"center",
+alignItems:"center"
+ }} >
+
+<Text> Profile </Text>
+
+</view>   
+    )
+}    
+}
